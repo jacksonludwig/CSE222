7227dfeedfecfbe4678b35b9aad0d57220c681d3
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2019-09-29 15:46:33 -0400

    hw2

---
+++ HW1Q2.asm
@@ -0,0 +1,17 @@
+
+.text
+
+main:
+    la		$a0, space		# 
+    
+    lui     $t0, 0x1122     # load 0x11223344 to register $t0
+    ori     $t0, $t0, 0x3344
+
+    sw		$t0, 0($a0)		# store contents in $t0 to memory address (($a0) + 0)
+    lb		$t1, 3($a0)		# load one byte from the memory to $t1
+
+    li		$v0, 10		# $v0 = 10
+    syscall
+
+.data
+space: .space 1024
